b5ecdce9ae3971cb7b59cd6e8449ce2ba5153ed7
R2D2 <jacob.turner870@gmail.com>
2021-04-02 01:55:36 +0200

    Appeasing the clippy gods

--- src/errors.rs
+++ src/errors.rs
@@ -4,7 +4,7 @@ use image::ImageError;
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("Unexpected io error in reading files: {}", .0)]
-    IO(#[from] std::io::Error),
+    Io(#[from] std::io::Error),
     #[error("Error encountered in image: {}", .0)]
     Image(#[from] ImageError),
     #[error("Cannot compute the Hamming distance of bitstrings of different lengths")]
